8eeb05b8024eacab4d77000983c63f822f3dacb5
Sukyeong Sung <skyeong258@gmail.com>
2025-02-19 15:49:42 +0900

    setting:error,global response

---
+++ src/main/java/cmf/commitField/global/exception/CustomException.java
@@ -0,0 +1,11 @@
+package cmf.commitField.global.exception;
+
+import cmf.commitField.global.error.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
--- src/main/java/cmf/commitField/global/jpa/BaseEntity.java
+++ src/main/java/cmf/commitField/global/jpa/BaseEntity.java
@@ -1,4 +1,26 @@
 package cmf.commitField.global.jpa;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @CreatedDate
+    @Getter
+    private LocalDateTime createdAt;
+
+    @CreatedDate
+    @Getter
+    private LocalDateTime modifiedAt;
 }
