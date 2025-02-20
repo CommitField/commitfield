6bbd0af1b0564817f8d36bc10006de3033df0f7a
whale22 <realwhale64@naver.com>
2025-02-20 17:23:20 +0900

    feat: 회원가입 기능 구현, DB 저장 확인

--- src/main/java/cmf/commitField/domain/user/entity/User.java
+++ src/main/java/cmf/commitField/domain/user/entity/User.java
@@ -2,14 +2,28 @@ package cmf.commitField.domain.user.entity;
 
 import cmf.commitField.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Builder;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
-class User extends BaseEntity {
-    private String email;
-    private String nickname;
-    private String password;
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+    private String username; // GitHub 로그인 ID
+    private String email; // 이메일
+    private String nickname; // 닉네임
+    private String avatarUrl; //아바타
+
+    @Enumerated(EnumType.STRING)  // DB에 저장될 때 String 형태로 저장됨
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
 }
