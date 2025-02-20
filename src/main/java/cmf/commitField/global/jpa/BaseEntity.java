314ca7be7c987997ca7e194a3306be17dd04eff2
Sukyeong Sung <skyeong258@gmail.com>
2025-02-20 10:28:32 +0900

    Add:BaseEntity 추가

--- src/main/java/cmf/commitField/global/jpa/BaseEntity.java
+++ src/main/java/cmf/commitField/global/jpa/BaseEntity.java
@@ -1,15 +1,27 @@
 package cmf.commitField.global.jpa;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
+@MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
