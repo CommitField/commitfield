970dcbca6747438f2fedf2911492bb5f41884c96
Sukyeong Sung <skyeong258@gmail.com>
2025-02-20 17:23:17 +0900

    Feat: chat entity 설정

---
+++ src/main/java/cmf/commitField/domain/chat/userChatRoom/entity/UserChatRoom.java
@@ -0,0 +1,29 @@
+package cmf.commitField.domain.chat.userChatRoom.entity;
+
+import cmf.commitField.domain.chat.chatRoom.entity.ChatRoom;
+import cmf.commitField.domain.user.entity.User;
+import cmf.commitField.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserChatRoom extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+}
