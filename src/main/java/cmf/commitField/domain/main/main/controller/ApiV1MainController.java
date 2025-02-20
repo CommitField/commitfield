e3f26b5cc928344c369ddd4851a4edfa4f394f4c
seoyeonson <eo970721@gmail.com>
2025-02-20 14:54:17 +0900

    fix : main controller annotation add

--- src/main/java/cmf/commitField/domain/main/main/controller/ApiV1MainController.java
+++ src/main/java/cmf/commitField/domain/main/main/controller/ApiV1MainController.java
@@ -1,9 +1,11 @@
 package cmf.commitField.domain.main.main.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @RequestMapping
 public class ApiV1MainController {
     @GetMapping
