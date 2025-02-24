ad6f53c1b62a413270a4bd074aa6bf3eb657171f
baeksunghyun <ces0135@naver.com>
2025-02-24 11:11:32 +0900

    Feat: 사용자와 레포지토리를 통해서 시즌 별 커밋 불러오기

--- src/main/java/cmf/commitField/domain/commit/sinceCommit/controller/SinceCommitController.java
+++ src/main/java/cmf/commitField/domain/commit/sinceCommit/controller/SinceCommitController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 @RestController
@@ -24,6 +25,69 @@ public class SinceCommitController {
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime since,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime until
             ) {
+        List<SinceCommitResponseDto> sinceCommits = sinceCommitService.getSinceCommits(owner, repo, since, until);
+
+        return ResponseEntity.ok(sinceCommits);
+    }
+
+    // 시즌 별 커밋 수 불러오기
+
+    // 봄 시즌
+    @GetMapping("/api/github/commits-spring")
+    public ResponseEntity<List<SinceCommitResponseDto>> getSpringSeasonCommits(
+            @RequestParam String owner,
+            @RequestParam String repo
+    ) {
+        int currentYear = LocalDateTime.now().getYear();
+        LocalDateTime since = LocalDateTime.of(2024, 3, 1, 0, 0);
+        LocalDateTime until = LocalDateTime.of(2024, 5, 31, 23, 59, 59);
+
+        List<SinceCommitResponseDto> sinceCommits = sinceCommitService.getSinceCommits(owner, repo, since, until);
+
+        return ResponseEntity.ok(sinceCommits);
+    }
+
+    // 여름 시즌
+    @GetMapping("/api/github/commits-summer")
+    public ResponseEntity<List<SinceCommitResponseDto>> getSummerSeasonCommits(
+            @RequestParam String owner,
+            @RequestParam String repo
+    ) {
+        int currentYear = LocalDateTime.now().getYear();
+        LocalDateTime since = LocalDateTime.of(currentYear, 6, 1, 0, 0);
+        LocalDateTime until = LocalDateTime.of(currentYear, 8, 31, 23, 59, 59);
+
+        List<SinceCommitResponseDto> sinceCommits = sinceCommitService.getSinceCommits(owner, repo, since, until);
+
+        return ResponseEntity.ok(sinceCommits);
+    }
+
+    // 가을 시즌
+    @GetMapping("/api/github/commits-fall")
+    public ResponseEntity<List<SinceCommitResponseDto>> getFallSeasonCommits(
+            @RequestParam String owner,
+            @RequestParam String repo
+    ) {
+        int currentYear = LocalDateTime.now().getYear();
+        LocalDateTime since = LocalDateTime.of(currentYear, 9, 1, 0, 0);
+        LocalDateTime until = LocalDateTime.of(currentYear, 11, 30, 23, 59, 59);
+
+        List<SinceCommitResponseDto> sinceCommits = sinceCommitService.getSinceCommits(owner, repo, since, until);
+
+        return ResponseEntity.ok(sinceCommits);
+    }
+
+    // 겨울 시즌
+    @GetMapping("/api/github/commits-winter")
+    public ResponseEntity<List<SinceCommitResponseDto>> getWinterSeasonCommits(
+            @RequestParam String owner,
+            @RequestParam String repo
+    ) {
+        int currentYear = LocalDateTime.now().getYear();
+        LocalDateTime since = LocalDateTime.of(currentYear, 12, 1, 0, 0);
+        LocalDateTime until = LocalDateTime.of(currentYear + 1, 2, 1, 23, 59, 59)
+                .with(TemporalAdjusters.lastDayOfMonth()); // 윤년 고려
+
         List<SinceCommitResponseDto> sinceCommits = sinceCommitService.getSinceCommits(owner, repo, since, until);
 
         return ResponseEntity.ok(sinceCommits);
