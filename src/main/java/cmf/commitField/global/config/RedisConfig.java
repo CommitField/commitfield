f49749770cb8f47da3434f1097397c611e486d0c
whale22 <realwhale64@naver.com>
2025-02-26 12:09:06 +0900

    fix: 오타 수정

--- src/main/java/cmf/commitField/global/config/RedisConfig.java
+++ src/main/java/cmf/commitField/global/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -34,6 +35,7 @@ public class RedisConfig {
         configuration.setHostName(host);
         configuration.setPort(port);
         return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -41,5 +43,4 @@ public class RedisConfig {
         template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
-
 }
