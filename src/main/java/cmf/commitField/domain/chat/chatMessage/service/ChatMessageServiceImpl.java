f5713afc2ee8230fb4eb331b765b5eed8e3e8b18
baeksunghyun <ces0135@naver.com>
2025-03-07 17:28:43 +0900

    Fix: Chat WebSocket 설정 변경

--- src/main/java/cmf/commitField/domain/chat/chatMessage/service/ChatMessageServiceImpl.java
+++ src/main/java/cmf/commitField/domain/chat/chatMessage/service/ChatMessageServiceImpl.java
@@ -48,14 +48,19 @@ public class ChatMessageServiceImpl implements ChatMessageService {
                 .user(findUser)
                 .chatRoom(chatRoom)
                 .build();
-        // Response
+
+        // 메시지 저장
+        ChatMsg savedMsg = chatMessageRepository.save(chatMsg);
+
+        // Response - 메시지 ID 추가
         ChatMsgResponse response = ChatMsgResponse.builder()
+                .chatMsgId(savedMsg.getId()) // 저장된 메시지의 ID 추가
                 .roomId(roomId)
                 .from(findUser.getNickname())
                 .message(message.getMessage())
                 .sendAt(chatMsg.getCreatedAt())
                 .build();
-        chatMessageRepository.save(chatMsg);
+
         return response;
     }
 
