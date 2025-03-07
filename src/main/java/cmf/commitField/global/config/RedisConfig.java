1f5689e7bc9529c4ba76cb06bf6d8702d30ac435
whale22 <realwhale64@naver.com>
2025-03-07 17:25:55 +0900

    fix: 주석 추가

--- src/main/java/cmf/commitField/global/config/RedisConfig.java
+++ src/main/java/cmf/commitField/global/config/RedisConfig.java
@@ -28,6 +28,7 @@ public class RedisConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
+        //로컬 환경에서 .setAddress("redis://127.0.0.1:6379")로 수정 필요
         config.useSingleServer()
                 .setAddress("redis://172.17.0.1:6379")
                 .setPassword(password); // 비밀번호 추가
