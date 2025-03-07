78d01324e1ad7d32f8090353f783882861ec3fc5
Sukyeong Sung <skyeong258@gmail.com>
2025-03-07 09:58:32 +0900

    Fix: Config 수정

--- src/main/java/cmf/commitField/global/security/WebConfig.java
+++ src/main/java/cmf/commitField/global/security/WebConfig.java
@@ -2,6 +2,7 @@ package cmf.commitField.global.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,6 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // @LoginCheck가 붙은 메서드에 대해 인터셉터를 적용
         registry.addInterceptor(loginCheckInterceptor)
-                .addPathPatterns("/chat/**");  // /chat 경로에만 인터셉터 적용
+                .addPathPatterns("/chat/**", "/uploads/**");  // /chat 경로에만 인터셉터 적용
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /uploads/** 경로가 실제 파일 시스템에서 제공되도록 설정
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:src/main/resources/static/uploads/");
     }
 }
