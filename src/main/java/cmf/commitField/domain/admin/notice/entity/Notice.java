5261719fb9d82c9e4f7a8c0c64b701f22dfac11e
seoyeonson <eo970721@gmail.com>
2025-04-21 18:09:30 +0900

    test: gemini 리뷰 테스트

--- src/main/java/cmf/commitField/domain/admin/notice/entity/Notice.java
+++ src/main/java/cmf/commitField/domain/admin/notice/entity/Notice.java
@@ -13,6 +13,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice extends BaseEntity {
+    // 관리자 공지사항
     private String title;
     private String content;
 }
